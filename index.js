a33950baa78a9a8c4a4211465d0da64fa697e6a3
harman <harmankaur5879@gmail.com>
2023-07-31 11:07:24 +0530

    update the  javascript code
    update the  javascript code

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ function validateform(){
     var inch=document.forms['MyForm']['user_inch'].value;
     var weight=document.forms['MyForm']['user_weight'].value;
 
-    var NumberPattern=/^(?:1[0-1][0-9]|[1-9]?[0-9])$/;
 
     var age_error=document.getElementById("AgeError");
     var weight_error=document.getElementById("WeightError");
+    var  height_error_ft=document.getElementById("height_Error_ft");
+    var  height_Error_in=document.getElementById("height_Error_in");
     
 
 
@@ -17,11 +18,6 @@ function validateform(){
         age_error.style.display="block";
         age_error.style.color="red";
          flag = false;
-    }else if(!age.match(NumberPattern)){
-        age_error.style.display="block";
-        age_error.style.color="red";
-        flag = false;
-
     }else if(age >= 1 && age<=100)
     {
         age_error.style.display="none";
@@ -33,28 +29,49 @@ function validateform(){
         flag = false;
     }
 
+    if(foot==""){
+        height_error_ft.style.display="block";
+        height_error_ft.style.color="red";
+        flag = false;
+
+    }else if(foot<=9){
+        height_error_ft.style.display="none";
+    }
+    else{
+        height_error_ft.style.display="block";
+        height_error_ft.style.color="red";
+        flag = false;
+    }
+     
+    if(inch==""){
+        height_Error_in.style.display="block";
+        height_Error_in.style.color="red";
+        flag = false;
+    }else if(inch<=12){
+        height_Error_in.style.display="none";
+    }else{
+        height_Error_in.style.display="block";
+        height_Error_in.style.color="red";
+    }
 
     if(weight==""){
         weight_error.style.display="block";
+        weight_error.style.color="red";
+
         flag = false;
-
-    }else if(!weight.match(NumberPattern)){
-        weight_error.style.display="block";
-        flag = false;
-
-
     }else if(weight >=1 && weight <=200){
         weight_error.style.display="none";
     }
     else{
         weight_error.style.display="block";
+        weight_error.style.color="red";
         flag = false;
     }
     return flag;
+}
 
 
 
     
 
-}
-console.log("hello");
+    // console.log(flag);
